d54c37df969bcac00ac758fd9e236e451e600778
Eric Humphries <32630651+letItBezhumphries@users.noreply.github.com>
2020-11-08 11:28:31 -0800

    Create instance-reviews.tf

---
+++ instance-reviews.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "reviews-service" {
+  ami           = var.AMI_ID
+  instance_type = "t2.micro"
+
+  # the VPC subnet
+  subnet_id = aws_subnet.main-public-1.id
+
+  # the security group
+  vpc_security_group_ids = [aws_security_group.reviews-sg.id]
+
+  # the public SSH key
+  key_name = aws_key_pair.mykeypair.key_name
+
+  # User-data
+  user_data = data.template_cloudinit_config.cloudinit-install-script.rendered
+  
+}
